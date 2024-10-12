e0a92d402d16712c7e6649d237578b4528f6dd65
Nurudeen <abdulmajeednurudeen48@gmail.com>
2024-10-12 11:45:37 +0000

    add delete controller and route for comments

--- src/controllers/commentsController.js
+++ src/controllers/commentsController.js
@@ -19,8 +19,6 @@ const createComment = [
       const { postId } = req.params;
       const userId = req.user.id;
 
-      console.log(`This is the post ID: ${postId}`);
-
       const post = await prisma.post.findUnique({
         where: { id: parseInt(postId, 10) },
         include: { comments: true },
@@ -53,4 +51,99 @@ const createComment = [
   },
 ];
 
-export { createComment };
+// Update comment
+const updateComment = [
+  validateComment(),
+
+  async (req, res, next) => {
+    // Get validation result from express-validator middleware
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const { content } = req.body;
+      const { postId, commentId } = req.params;
+      const userId = req.user.id;
+
+      console.log(
+        `Updating comment with ID: ${commentId} for post ID: ${postId}`
+      );
+
+      // Check if the post exists
+      const post = await prisma.post.findUnique({
+        where: { id: parseInt(postId, 10) },
+        include: { comments: true },
+      });
+      if (!post) {
+        return res.status(404).json({ message: 'Post not found' });
+      }
+
+      // Check if the comment exists
+      const comment = await prisma.comment.findUnique({
+        where: { id: parseInt(commentId, 10) },
+      });
+      if (!comment) {
+        return res.status(404).json({ message: 'Comment not found' });
+      }
+
+      if (comment.userId !== userId) {
+        return res.status(403).json({
+          message: 'You do not have permission to update this comment',
+        });
+      }
+
+      // Update the comment in the database
+      const updatedComment = await prisma.comment.update({
+        where: { id: parseInt(commentId, 10) },
+        data: { content },
+      });
+
+      res.json({ message: 'Comment updated successfully!', updatedComment });
+    } catch (error) {
+      console.error('Error updating comment', error);
+      next(error);
+    }
+  },
+];
+
+// Delete comment
+const deleteComment = async (req, res, next) => {
+  try {
+    const { postId, commentId } = req.params;
+    const userId = req.user.id;
+
+    console.log(
+      `Deleting comment with ID: ${commentId} for post ID: ${postId}`
+    );
+
+    // Check if the post exists
+    const post = await prisma.post.findUnique({
+      where: { id: parseInt(postId, 10) },
+    });
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' });
+    }
+
+    // Check if the comment exists
+    const comment = await prisma.comment.findUnique({
+      where: { id: parseInt(commentId, 10) },
+    });
+    if (!comment) {
+      return res.status(404).json({ message: 'Comment not found' });
+    }
+
+    // Delete the comment
+    await prisma.comment.delete({
+      where: { id: parseInt(commentId, 10) },
+    });
+
+    res.json({ message: 'Comment deleted successfully!' });
+  } catch (error) {
+    console.error('Error deleting comment', error);
+    next(error);
+  }
+};
+
+export { createComment, updateComment, deleteComment };
--- src/routes/comments.js
+++ src/routes/comments.js
@@ -3,7 +3,7 @@ const commentsRoute = Router();
 
 import authLimiter from '../middleware/rateLimit.js';
 import authenticateJWT from '../authentication/jwtAuthentication.js';
-import * as postController from '../controllers/commentsController.js';
+import * as commentsController from '../controllers/commentsController.js';
 
 /**
  * Route for creating a comment.
@@ -12,7 +12,27 @@ commentsRoute.post(
   '/comment/posts/:postId',
   authenticateJWT,
   authLimiter,
-  postController.createComment
+  commentsController.createComment
+);
+
+/**
+ * Route for updating a comment.
+ */
+commentsRoute.put(
+  '/comment/posts/:postId/:commentId',
+  authenticateJWT,
+  authLimiter,
+  commentsController.updateComment
+);
+
+/**
+ * Route for deleting a comment.
+ */
+commentsRoute.delete(
+  '/comment/posts/:postId/:commentId',
+  authenticateJWT,
+  authLimiter, // Rate limiting middleware
+  commentsController.deleteComment
 );
 
 export default commentsRoute;
