0d8383f5721c7ac7f767f0e46d071cb678476801
Nurudeen Abdul-Majeed <abdulmajeednurudeen48@gmail.com>
2025-02-01 19:35:43 +0000

    Removed user password from the user controller to be sent to client

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,19 +2,23 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 const userData = async (req, res) => {
-  const email = req.user.email;
-
-  if (!email) {
-    return res.status(401).json({ message: 'Unauthenticated, please login.' });
-  }
-
-  // Find user by email
-  const user = await prisma.user.findUnique({
-    where: { email: email },
-  });
-
   try {
-    res.json({ user });
+    const email = req.user.email;
+
+    if (!email) {
+      return res
+        .status(401)
+        .json({ message: 'Unauthenticated, please login.' });
+    }
+
+    // Find user by email
+    const user = await prisma.user.findUnique({
+      where: { email: email },
+    });
+
+    const { password, ...rest } = user;
+
+    res.json({ user: rest });
   } catch (error) {
     console.error('Error retrieving user from token:', error);
     res.status(500).json({ message: 'Server error' });
